5db49cab0b43e8c5cbdd1b306b9a76830c73f2e9
Lucas Huysmans <lucashuysmans@Lucass-MacBook-Pro.local>
2020-12-07 22:28:01 +0000

    fixed cython setup, for combinations function

--- cython_module.pyx
+++ cython_module.pyx
@@ -1,22 +1,40 @@
-cimport math as cm
-cimport numpy as cnp
+import numpy as np
+cimport numpy as np
+cdef int comb(int n,int k):
+    if k>n:
+        return 0
+    if k*2>n:
+        k=n-k
+    if k==0:
+        return 1
+    
+    cdef int result=n
+    cdef unsigned int i
+    for i in range(2,k+1):
+        result *= n-i+1
+        result /=i
+    return result
 def combinations(int n,int m,int f=1):
     """m*l array which give the distinct m-tuples of numbers 0 to n-1 with offset f"""
-    if (m-1)*f>m:
+    if (m-1)*f>n:
         print('no tuples exist')
+        return
     cdef int c
-    c=cm.comb(n+(1-f)*m,m)
-    cdef cnp.ndarray[cnp.int_t, ndim=2] A=cnp.empty((m,c),dtype=cnp.int)
+    c=comb(n-(f-1)*(m-1),m)
+    cdef np.ndarray[np.int_t, ndim=2] A=np.empty((m,c),dtype=np.int)
     cdef unsigned int i, j, k
     for j in range(0,m):
-        A[0,j]=0
-    cdef int d=n+(m-1)*f-1
+        A[j,0]=0
+    cdef unsigned int d=n-(m-1)*f-1
     for i in range(1,c):
-        for j in range(0,m,-1):
-            if A[i-1,j]<d:
-                for k in range(0,j-1):
-                    A[i,k]=A[i-1,k]
+        for j in range(m-1,-1,-1):
+            if A[j,i-1]<d:
+                for k in range(0,j):
+                    A[k,i]=A[k,i-1]
                 for k in range(j,m):
-                    A[i,k]=A[i-1,j]+1
+                    A[k,i]=A[j,i-1]+1
                 break
-    return A+cnp.arrange(0,m*f,f)[:,None]*cnp.ones((m,c),dtype=cnp.int)
+    if f==0:
+        return A
+    else:
+        return A+np.arange(0,m*f,f)[:,None]*np.ones((m,c),dtype=np.int)
--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy as np
+
+extensions=[Extension("cython_module",["cython_module.pyx"],include_dirs=[np.get_include()])]
 
 setup(
-    ext_modules = cythonize("cython_module.pyx")
+    cmdclass={'build_ext':build_ext},
+    ext_modules = extensions
 )
